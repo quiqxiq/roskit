030fa857dfb8a5dd420eb60c0ec3144d0fac0f1c
Butterfly-Student <entertainmenta654@gmail.com>
2026-04-16 21:08:48 +0700

    first

---
+++ include/config.php
@@ -0,0 +1,5 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+
+$data['cek'] = array ('1'=>'cek!10.16.30.1:200','cek@|@mimon','cek#|#aWJkag==','cek%etre','cek^rtre.v','cek&Rp','cek*10','cek(1','cek)','cek=10','cek@!@enable');
